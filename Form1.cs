ae3d1704e61965f89e99076822c43288483da0ae
brenekv.03 <brenekv.03@A321-15>
2023-01-26 11:27:56 +0100

    Přidejte soubory projektu.

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metody02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        static private void UlozitDoPole(double[] pole,TextBox textBox)
+        {
+            for(int i = 0; i < textBox.Lines.Length; i++)
+            {
+                pole[i] = int.Parse(textBox.Lines[i]);
+            }
+        }
+        static private void ZobrazitPole(double[] pole, ListBox listbox)
+        {
+            foreach(double x in pole)
+            {
+                listbox.Items.Add(x);
+            }
+        }
+        static private bool JeRostouci(double[] pole)
+        {
+            double prvniCislo = pole[0];
+            bool jeRostouci = true;
+            for( int i = 0; i <pole.Length&&jeRostouci;i++)
+            {
+                if (pole[i]>)
+            }
+        }
+    }
+}
